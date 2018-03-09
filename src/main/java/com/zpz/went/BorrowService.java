f8bd5d651778a087fe1058b5d23301dcfb10e8c6
zzz <897035440@qq.com>
2018-03-09 10:11:59 +0800

    修复目录结构错误

---
+++ src/main/java/com/zpz/went/BorrowService.java
@@ -0,0 +1,38 @@
+package com.zpz.went;
+
+import com.jfinal.kit.Ret;
+import com.jfinal.plugin.activerecord.Db;
+
+import java.util.List;
+
+/**
+ * Created by home on 2018/3/8.
+ */
+public class BorrowService {
+    public static final BorrowService me=new BorrowService();
+    private static final Book bookDao=new Book().dao();
+    private static final Borrow borrowDao = new Borrow().dao();
+    private static final User userDao = new User().dao();
+
+
+    public List<Book> getBookList(){
+        List<Book> bookList=null;
+        bookList=bookDao.find(Db.getSqlPara("book.findBook"));
+        return bookList;
+    }
+
+    public Ret BorrowBook(Integer bookId){
+        Book book=bookDao.findFirst(Db.getSqlPara("book.findById",bookId));
+        if(book.getNumb()==0){
+            return Ret.by("status",false).set("message","没有库存了");
+        }else{
+            if(book.setNumb(book.getNumb()-1).update()){
+
+                return Ret.by("status",true);
+            } else {
+
+                return Ret.by("status",false).set("message","错误");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/zpz/went/SignValidator.java
@@ -0,0 +1,21 @@
+package com.zpz.went;
+
+import com.jfinal.core.Controller;
+
+/**
+ * Created by home on 2018/3/8.
+ */
+public class SignValidator extends BaseValidator {
+
+    @Override
+    protected void validate(Controller c) {
+        validateEqualString(c.getPara("password"), c.getPara("repeatPassword"), "message",
+                "两次密码不一致");
+    }
+
+    @Override
+    protected void handleError(Controller c) {
+        c.setAttr("status",false);
+        c.renderJson();
+    }
+}
