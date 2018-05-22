93a06007dd5bbceb54995024ce6f952755ea824e
zzz <897035440@qq.com>
2018-05-22 12:30:08 +0800

    服务器端代码

---
+++ src/main/java/com/zpz/went/common/interceptor/RoleInterceptor.java
@@ -0,0 +1,25 @@
+package com.zpz.went.common.interceptor;
+
+import com.jfinal.aop.Interceptor;
+import com.jfinal.aop.Invocation;
+import com.jfinal.core.Controller;
+import com.jfinal.plugin.redis.Redis;
+import com.zpz.went.common.model.User;
+import com.zpz.went.login.LoginService;
+
+/**
+ * Created by home on 2018/3/12.
+ */
+public class RoleInterceptor implements Interceptor {
+    @Override
+    public void intercept(Invocation inv) {
+        Controller c = inv.getController();
+        String token = c.getCookie(LoginService.SESSION_ID_NAME);
+        User user = Redis.use().get(token);
+        if (user.getType() == 0) {
+            c.redirect("/");
+            return;
+        }
+        inv.invoke();
+    }
+}
